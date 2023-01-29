f80f40ba620ccb8c34ca6ab6617615e71ec77eca
MichSeman <89609365+MichSeman@users.noreply.github.com>
2023-01-29 17:25:37 -0500

    html and css updates

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./Weather.css";
 
 function Weather() {
-    return <div className="Weather"> Hello from Weather Station
+    return <div className="Weather"> 
+    <h1>Athens</h1>
+    <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+    </ul>
+    Hello from Weather Station
     </div>
 }
 
