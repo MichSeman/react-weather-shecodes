4ad9187f5f38ec820aee6d7e73ea674f1c0dba9e
MichSeman <89609365+MichSeman@users.noreply.github.com>
2023-01-29 16:59:56 -0500

    created the react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Weather App</h1> 
+      <footer>
+        This project is created by {" "}
+        <a href='https://astonishing-bombolone-2e17ca.netlify.app/'  target="_blank" rel="noreferrer">
+          Michelle Seman
+        </a> 
+          {" "} and is
+        <a href='https://github.com/MichSeman/react-weather-shecodes.git' target="_blank" rel="noreferrer"> open-sources on GitHub</a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
