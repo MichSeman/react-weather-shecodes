5a40a0a80790510f207e2f855f2fff5960c6d8ab
MichSeman <89609365+MichSeman@users.noreply.github.com>
2023-01-29 18:38:00 -0500

    added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -6,10 +6,16 @@ function Weather() {
     <form>
         <div className="row">
             <div className="col-9">
-                <input type="search" placeholder="enter a city" className="form-control"/>
+                <input type="search" 
+                placeholder="enter a city" className="form-control"/>
             </div>
             <div className="col-3">
-                <input type="submit" value="Search" className="btn btn-warning"/>
+                <input 
+                type="submit"
+                 value="Search" 
+                 className="btn btn-warning w-100"
+                autoFocus="on"/>
+                {/* w-100 makes the btn full width */}
             </div>
         </div>
     </form>
@@ -18,15 +24,18 @@ function Weather() {
         <li>Wednesday 07:00</li>
         <li>Mostly Cloudy</li>
     </ul>
-    <div className="row">
+    <div className="row mt-3">
         <div className="col-6">
-            <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="mostly cloudy">
-            </img>
-            6 degrees
+            <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
+            alt="mostly cloudy"
+            className="float-left" />
+           
+            <span className="temperature">6 </span>
+            <span className="unit">°C</span>
         </div>
         <div className="row">
         <div className="col-6">
-            <ul>
+            <ul className="weather-extras">
                 <li>percipitation</li>
                 <li>humidity </li>
                  <li>wind</li>
