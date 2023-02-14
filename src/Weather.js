de5b51560bf08a51a91a176bb440aeb3e81789ca
MichSeman <89609365+MichSeman@users.noreply.github.com>
2023-02-14 15:12:17 -0500

    Update Weather.js
    created new API key on open weather source to fix the error "too many requests" on the previous API

--- src/Weather.js
+++ src/Weather.js
@@ -32,7 +32,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    const apiKey = "992bb76f534838ab44ffcd33d0d2bd7e";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(handleResponse);
   }
